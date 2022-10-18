3566c40de56dc2b5ccc969c0a49a0f4e17f54a90
NogueiraMatos <marcos.nogueira85@gmail.com>
2022-10-18 18:16:07 -0300

    Ajustes html

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 "use strict";
-const curso = "TypeScript";
-console.log(curso);
+document.addEventListener("DOMContentLoaded", function () {
+    eventListeners();
+});
+function eventListeners() {
+    const newTaskButton = document.getElementById('new-task');
+    newTaskButton.addEventListener("click", newTaskPopUpShow);
+}
+function newTaskPopUpShow() {
+    const newTaskPopUp = document.getElementById('new-task-popup');
+    newTaskPopUp.classList.remove('hidden');
+}
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,2 +1,13 @@
-const curso: string = "TypeScript";
-console.log(curso);
+document.addEventListener("DOMContentLoaded", function () {
+    eventListeners()
+})
+
+function eventListeners() {
+    const newTaskButton = document.getElementById('new-task')
+    newTaskButton!.addEventListener("click", newTaskPopUpShow)
+}
+
+function newTaskPopUpShow() {
+  const newTaskPopUp = document.getElementById('new-task-popup')
+  newTaskPopUp!.classList.remove('hidden')
+}
